6b2ac19ee50f83b9510749b341bb906fa3abd3ac
Developer <developer@example.com>
2025-12-06 17:20:31 +0500

    Доработана телефонная книга: 1. Добавлен ввод размера массива 2. Исправлена проверка пунктов меню от 0-4 3. Исправлен ввод имени и номера (теперь " " не пройдет) 4. Добавлена проверка телефона(размешен формат + и 7-15 цифр). 5. Сделал отдельный метод по выходу в меню (код стал чище) 6. Исправлено удаление контактов. (смещение + зачистка)

--- src/main/java/school/sorokin/javacore/basics/BasicFinalTask.java
+++ src/main/java/school/sorokin/javacore/basics/BasicFinalTask.java
@@ -3,19 +3,39 @@ package school.sorokin.javacore.basics;
 import java.util.Scanner;
 
 public class BasicFinalTask {
+
+    public static void waitForZero(Scanner console) {
+
+        String input;
+
+        do {
+            System.out.println("Нажмите 0, чтобы вернуться в меню:");
+            input = console.nextLine().trim();
+
+            if (!"0".equals(input)) {
+                System.out.println("Ошибка: нужно нажать именно 0.\n");
+            }
+        } while (!"0".equals(input));
+    }
+
+
     public static void main(String[] args) {
 
         //Инициализируем сканер и создаем два массива имя и телефон.
         Scanner console = new Scanner(System.in);
-        String[] names = new String[2];
-        String[] phones = new String[2];
+        System.out.println("Решите, какой будет максимальный размер контактов (например, 100 контактов)");
+        int size = console.nextInt();
+
+        String[] names = new String[size];
+        String[] phones = new String[size];
         String menu;
+        int choice;
         boolean checkFlag = false;
 
 
         //Делаем цикличное меню, где пользователь делает выбор.
         while(true) {
-            System.out.println("ТЕЛЕФОННАЯ КНИГА");
+            System.out.println("ТЕЛЕФОННАЯ КНИГА СОЗДАНА");
             System.out.println();
             System.out.println("1.Добавление нового контакта.");
             System.out.println("2.Просмотр всех контактов.");
@@ -26,23 +46,15 @@ public class BasicFinalTask {
             System.out.println("Введите число:");
             System.out.println();
 
-            int choice = console.nextInt();
-            console.nextLine();
-
-
             //Проверяем корретность выбора
-            while(choice < 0 || choice > 4 ){
-                System.out.println("Не корректный выбор. Попробуйте еще раз: ");
-                System.out.println("1.Добавление нового контакта.");
-                System.out.println("2.Просмотр всех контактов.");
-                System.out.println("3.Поиск контакта по имени.");
-                System.out.println("4.Удаление контакта");
-                System.out.println("0.Выход из программы:");
-                System.out.println();
-
+            while(true){
+                System.out.println("Введите число от 0 до 4");
                 choice = console.nextInt();
                 console.nextLine();
-                System.out.println();
+                if(choice >= 0 && choice <= 4){
+                    break;
+                }
+                System.out.println("Ошибка: Число должно быть от 0 до 4.");
 
             }
 
@@ -66,16 +78,40 @@ public class BasicFinalTask {
                     }
 
                     //Если свободное место есть - создаем новый контакт.
-                    System.out.println("ДОБАВЛЕНИЕ НОВОГО КОНТАКТА:");
-                    System.out.println();
+                    System.out.println("ДОБАВЛЕНИЕ НОВОГО КОНТАКТА:\n");
+                    String name;
+                    String phone;
 
-                    System.out.println("Введите имя: ");
-                    String name = console.nextLine();
-                    System.out.println();
+                    while(true) {
+                        System.out.println("Введите имя: ");
+                        name = console.nextLine().trim();
 
-                    System.out.println("Введите телефон: ");
-                    String phone = console.nextLine();
-                    System.out.println();
+                        if(!name.isEmpty()){
+                            break;
+                        }
+
+                        System.out.println("Ошибка: имя не может быть пустым. \n");
+
+                    }
+
+
+                    while(true) {
+                        System.out.println("Введите телефон: ");
+                        phone = console.nextLine().trim();
+
+                        if(phone.isEmpty()){
+                            System.out.println("Ошибка: телефон не может быть пустым. \n");
+                            continue;
+                        }
+
+                        if(phone.matches("^\\+?\\d{7,15}$")){
+                            break;
+                        }else{
+                            System.out.println("Ошибка: телефон должен содержать только цифры (можно через + в начале, длинна 7-15 символов).");
+                        }
+                    }
+
+
 
 
                     for (int i = 0; i < names.length; i++) {
@@ -86,17 +122,8 @@ public class BasicFinalTask {
                             break;
                         }
                     }
-
                     System.out.println();
-                    System.out.println("Нажмите 0, чтобы вернуться в меню.");
-
-                    do{
-                        menu = console.nextLine();
-                        if(!menu.equals("0")){
-                        System.out.println("Неизвестная цифра, попробуйте еще раз");
-                        System.out.println("Нажмите 0, чтобы вернуться в меню.");
-                        }
-                    } while(!menu.equals("0"));
+                    waitForZero(console);
                     break;
 
                 //Пользовватель выбрал "СПИСОК ВСЕХ КОНТАКТОВ".
@@ -118,14 +145,7 @@ public class BasicFinalTask {
                     }
 
                     System.out.println();
-                    System.out.println("Нажмите 0, чтобы вернуться в меню.");
-                    do{
-                        menu = console.nextLine();
-                        if(!menu.equals("0")){
-                            System.out.println("Неизвестная цифра, попробуйте еще раз");
-                            System.out.println("Нажмите 0, чтобы вернуться в меню.");
-                        }
-                    } while(!menu.equals("0"));
+                    waitForZero(console);
                     break;
 
                 //Пользовватель выбрал "ПОИСК КОНТАКТА ПО ИМЕНИ".
@@ -144,15 +164,9 @@ public class BasicFinalTask {
 
                     if(!checkFlag){
                         System.out.println("Нет контактов для поиска.");
+
                         System.out.println();
-                        System.out.println("Нажмите 0, чтобы вернуться в меню.");
-                        do{
-                            menu = console.nextLine();
-                            if(!menu.equals("0")){
-                                System.out.println("Неизвестная цифра, попробуйте еще раз");
-                                System.out.println("Нажмите 0, чтобы вернуться в меню.");
-                            }
-                        } while(!menu.equals("0"));
+                        waitForZero(console);
                         break;
                     }
 
@@ -180,14 +194,7 @@ public class BasicFinalTask {
                     }
                     System.out.println();
 
-                    System.out.println("Нажмите 0, чтобы вернуться в меню.");
-                    do{
-                        menu = console.nextLine();
-                        if(!menu.equals("0")){
-                            System.out.println("Неизвестная цифра, попробуйте еще раз");
-                            System.out.println("Нажмите 0, чтобы вернуться в меню.");
-                        }
-                    } while(!menu.equals("0"));
+                    waitForZero(console);
                     break;
 
                 //Пользовватель выбрал "УДАЛЕНИЕ КОНТАКТА".
@@ -207,14 +214,7 @@ public class BasicFinalTask {
                     if(!checkFlag){
                         System.out.println("Нет контактов для удаления.");
                         System.out.println();
-                        System.out.println("Нажмите 0, чтобы вернуться в меню.");
-                        do{
-                            menu = console.nextLine();
-                            if(!menu.equals("0")){
-                                System.out.println("Неизвестная цифра, попробуйте еще раз");
-                                System.out.println("Нажмите 0, чтобы вернуться в меню.");
-                            }
-                        } while(!menu.equals("0"));
+                       waitForZero(console);
                         break;
                     }
 
@@ -222,14 +222,23 @@ public class BasicFinalTask {
                     // перебираем запись в массиве с проверкой содержания текста ->
                     // если нашли удаляем запись, если нет - выводим "Нет контакта с таким именем." -> выходим в главное меню.
                     System.out.println("Введите имя контакта: ");
-                    String deleteContact = console.nextLine();
+                    String deleteContact = console.nextLine().trim();
                     System.out.println();
 
                     checkFlag = false;
+
                     for(int i = 0; i < names.length; i++){
-                        if(names[i] != null && names[i].contains(deleteContact)){
-                            names[i] = null;
-                            phones[i] = null;
+                        if(names[i] != null && names[i].equalsIgnoreCase(deleteContact)){
+                            //Смещаем элементы влево
+                            for (int j = i; j< names.length - 1; j++){
+                                names[j] = names[j + 1];
+                                phones[j] = phones[j +1];
+                            }
+
+                            //Чистим последние элементы
+                            names[names.length - 1] = null;
+                            phones[phones.length - 1] = null;
+
                             checkFlag = true;
                             System.out.println("Контакт удален.");
                             break;
@@ -241,14 +250,7 @@ public class BasicFinalTask {
                     }
                     System.out.println();
 
-                    System.out.println("Нажмите 0, чтобы вернуться в меню.");
-                    do{
-                        menu = console.nextLine();
-                        if(!menu.equals("0")){
-                            System.out.println("Неизвестная цифра, попробуйте еще раз");
-                            System.out.println("Нажмите 0, чтобы вернуться в меню.");
-                        }
-                    } while(!menu.equals("0"));
+                    waitForZero(console);
                     break;
 
 
