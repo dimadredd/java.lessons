0dd3384ff0800903388eba26a1279d8850cc5be5
Developer <developer@example.com>
2025-12-09 23:11:22 +0500

    алькулятор облигаций демо

--- src/main/java/school/sorokin/javacore/App.java
+++ src/main/java/school/sorokin/javacore/App.java
@@ -1,13 +1,35 @@
 package school.sorokin.javacore;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+public class App {
+
+    static double price = 595.44;
+    static double couponePrice = 70.8;
+    static int sheets = (int) ((int) 100000 / price);
+    static double money = 10000;
+    static int time = 5;
+    static double investTime = money * 52;
+
+
+    public static void main(String[] args) {
+        for(int i = 1; i <= time; i++) {
+
+            //Пополнение на 60 000
+            sheets += investTime / price;
+
+            //Купонный доход
+            double couponRevenue = sheets * couponePrice;
+
+            //Реинвест
+            double reinvest = couponRevenue / price;
+            sheets += reinvest;
+
+            double totalMoney = sheets * 1000;
+
+            System.out.println("Итерация " + i);
+            System.out.println("Активов: " + sheets);
+            System.out.println("Денег: " + totalMoney);
+            System.out.println("----------------------------");
+        }
     }
 }
